c2aadf3697f51a768690c979215caa1423addaed
hambyhacks <hambyhaxx@gmail.com>
2022-05-26 08:04:20 +0800

    added validation function for date_ordered and date_received.

--- tests/validation_test.go
+++ tests/validation_test.go
@@ -8,9 +8,12 @@ import (
 )
 
 func TestValidation(t *testing.T) {
+
 	p := models.Product{
 		TrackingNumber: "JTXPH000000000000",
 		Name:           "Test product",
+		DateOrdered:    "2022-05-30",
+		DateReceived:   "2022-06-30",
 	}
 
 	err := service.Validate(p)
